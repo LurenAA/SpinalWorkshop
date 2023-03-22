d8a0d9d4b6cceaf37d4ba9050a3179605a645ef5
xgbcat315 <978601499@qq.com>
2023-03-22 20:52:29 +0800

    finished functionUnit

--- src/main/scala/workshop/function/FunctionUnit.scala
+++ src/main/scala/workshop/function/FunctionUnit.scala
@@ -11,15 +11,41 @@ case class FunctionUnit() extends Component{
     val valueB = out Bits(32 bits)
     val valueC = out Bits(48 bits)
   }
-
+  
   def patternDetector(str : String) = new Area{
     val hit = False
     // TODO
+    val counter = Reg(UInt(log2Up(9) bits)) init(0)
+    when(io.cmd.valid ) {
+      when(io.cmd.payload === B"8'x73") {
+        counter := 1
+      }.elsewhen(counter < 8) {
+        counter := counter + 1
+      }.otherwise{
+        counter := 0
+      }
+    }
+    when(counter === 8 && str(8).toInt === io.cmd.payload) {
+      hit := True
+    }.otherwise{
+      hit := False
+    }
   }
 
-  def valueLoader(start : Bool,that : Data)= new Area{
+  def valueLoader(start : Bool,that : Bits)= new Area{
     require(widthOf(that) % widthOf(io.cmd.payload) == 0) //You can make the assumption that the 'that' width is always an mulitple of 8
     // TODO
+    val ncount = U(widthOf(that) / 8)
+    val counter = Reg(UInt(ncount.getWidth bits)) init(0)
+    val outthat = Reg(that) init(0)
+    that := outthat
+    when(start) {
+      counter := ncount.resized
+    }.elsewhen(counter > 0 && io.cmd.valid) {
+      counter := counter - 1
+      outthat((ncount - counter) * 8, 8 bits) := io.cmd.payload
+    }
+
   }
 
   val setA    = patternDetector("setValueA")
