24adddc8edb5922840b2a1f76352a7707611e433
xgbcat315 <978601499@qq.com>
2023-03-23 17:52:16 +0800

    finished apb3decoder

--- src/main/scala/workshop/apb3decoder/Apb3Decoder.scala
+++ src/main/scala/workshop/apb3decoder/Apb3Decoder.scala
@@ -31,7 +31,46 @@ case class Apb3Decoder(apbConfig : Apb3Config, outputsMapping : Seq[Mapping]) ex
     val input   = slave(Apb3(apbConfig))
     val outputs = Vec(master(Apb3(apbConfig)), outputsMapping.length)
   }
+  // io.input.PREADY := True
+  // io.input.PSLVERROR := False
+  // io.input.PRDATA := 0
+  // io.input.PRDATA := io.outputs(0).PRDATA
+  // val hitn = UInt(outputsMapping.length bits)
+  for(idx <- 0 until outputsMapping.length) {
+    io.outputs(idx).PADDR := io.input.PADDR
+    io.outputs(idx).PENABLE := io.input.PENABLE
+    io.outputs(idx).PWDATA := io.input.PWDATA
+    io.outputs(idx).PWRITE := io.input.PWRITE
 
+    when(outputsMapping(idx).hit(io.input.PADDR)) {
+      io.outputs(idx).PSEL := io.input.PSEL
+      // hitn(idx) := True
+    }.otherwise{
+      io.outputs(idx).PSEL := 0
+      // hitn(idx) := False
+    }
+  }
+  
+  when(outputsMapping(0).hit(io.input.PADDR)) {
+      io.input.PRDATA := io.outputs(0).PRDATA
+      io.input.PREADY := io.outputs(0).PREADY
+      io.input.PSLVERROR := io.outputs(0).PSLVERROR
+  }.elsewhen(outputsMapping(1).hit(io.input.PADDR)) {
+      io.input.PRDATA := io.outputs(1).PRDATA
+      io.input.PREADY := io.outputs(1).PREADY
+      io.input.PSLVERROR := io.outputs(1).PSLVERROR
+  }.elsewhen(outputsMapping(2).hit(io.input.PADDR)) {
+      io.input.PRDATA := io.outputs(2).PRDATA
+      io.input.PREADY := io.outputs(2).PREADY
+      io.input.PSLVERROR := io.outputs(2).PSLVERROR
+  }.elsewhen(outputsMapping(3).hit(io.input.PADDR)) {
+      io.input.PRDATA := io.outputs(3).PRDATA
+      io.input.PREADY := io.outputs(3).PREADY
+      io.input.PSLVERROR := io.outputs(3).PSLVERROR
+  }.otherwise{
+    io.input.PRDATA := io.outputs(0).PRDATA
+    io.input.PREADY := io.outputs(0).PREADY
+    io.input.PSLVERROR := io.outputs(0).PSLVERROR
+  }
   //TODO fully asynchronous apb3 decoder
 }
-
