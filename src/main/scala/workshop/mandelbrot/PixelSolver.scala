4f4cc54b50faf8f90f00f14e84df5411f9a743fa
xgbcat315 <978601499@qq.com>
2023-03-26 22:47:23 +0800

    finished PixelSolver

--- src/main/scala/workshop/mandelbrot/PixelSolver.scala
+++ src/main/scala/workshop/mandelbrot/PixelSolver.scala
@@ -28,6 +28,43 @@ case class PixelSolver(g : PixelSolverGenerics) extends Component{
     val rsp = master Stream(PixelResult(g))
   }
 
-  //TODO implement the mandelbrot algorithm
-}
+  val x = Reg(g.fixType) 
+  val y = Reg(g.fixType)
+  val x0 = Reg(g.fixType)
+  val y0 = Reg(g.fixType)
+  val iteration = Reg(g.iterationType) 
+  val two = Reg(g.fixType).allowUnsetRegToAvoidLatch 
+  two := 2
 
+  val start = Reg(Bool) init(False)
+  val validrsp = Reg(Bool) init(False)
+  io.rsp.valid := validrsp
+  
+  when(start && x*x + y*y < 4 && iteration < U(255).resized) {
+    x := (x*x - y*y + x0).truncated
+    y := (x*y*two + y0).truncated
+    iteration := iteration + 1
+  }.elsewhen(start) {
+    start := False
+    validrsp := True
+  }.otherwise{
+    start := False
+  }
+
+
+  io.cmd.ready := (!io.rsp.valid | io.rsp.ready) && !start
+  when(io.cmd.fire) {
+    start := True
+    x := 0.0
+    y := 0.0
+    iteration := 0
+    x0 := io.cmd.payload.x
+    y0 := io.cmd.payload.y
+  }
+
+  io.rsp.payload.iteration := iteration
+  
+  when(io.rsp.fire) {
+    validrsp := False
+  }
+}
