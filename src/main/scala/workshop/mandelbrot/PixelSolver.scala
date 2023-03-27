123a7dfef4e489a294d674cc6336ccdde9d7b28d
xgbcat315 <978601499@qq.com>
2023-03-27 16:14:08 +0800

    finished mandelbrot

--- src/main/scala/workshop/mandelbrot/PixelSolver.scala
+++ src/main/scala/workshop/mandelbrot/PixelSolver.scala
@@ -1,3 +1,73 @@
+// package workshop.mandelbrot
+
+// import spinal.core._
+// import spinal.lib._
+
+// case class PixelSolverGenerics(fixAmplitude : Int,
+//                                fixResolution : Int,
+//                                iterationLimit : Int){
+//   val iterationWidth = log2Up(iterationLimit+1)
+//   def iterationType = UInt(iterationWidth bits)
+//   def fixType = SFix(
+//     peak = fixAmplitude exp,
+//     resolution = fixResolution exp
+//   )
+// }
+
+// case class PixelTask(g : PixelSolverGenerics) extends Bundle{
+//   val x,y = g.fixType
+// }
+
+// case class PixelResult(g : PixelSolverGenerics) extends Bundle{
+//   val iteration = g.iterationType
+// }
+
+// case class PixelSolver(g : PixelSolverGenerics) extends Component{
+//   val io = new Bundle{
+//     val cmd = slave  Stream(PixelTask(g))
+//     val rsp = master Stream(PixelResult(g))
+//   }
+
+//   val x = Reg(g.fixType) 
+//   val y = Reg(g.fixType)
+//   val x0 = Reg(g.fixType)
+//   val y0 = Reg(g.fixType)
+//   val iteration = Reg(g.iterationType) 
+//   val two = Reg(g.fixType).allowUnsetRegToAvoidLatch 
+//   two := 2
+
+//   val start = Reg(Bool) init(False)
+//   val validrsp = Reg(Bool) init(False)
+//   io.rsp.valid := validrsp
+  
+//   when(start && x*x + y*y < 4 && iteration < U(255).resized) {
+//     x := (x*x - y*y + x0).truncated
+//     y := (x*y*two + y0).truncated
+//     iteration := iteration + 1
+//   }.elsewhen(start) {
+//     start := False
+//     validrsp := True
+//   }.otherwise{
+//     start := False
+//   }
+
+
+//   io.cmd.ready := (!io.rsp.valid | io.rsp.ready) && !start
+//   when(io.cmd.fire) {
+//     start := True
+//     x := 0.0
+//     y := 0.0
+//     iteration := 0
+//     x0 := io.cmd.payload.x
+//     y0 := io.cmd.payload.y
+//   }
+
+//   io.rsp.payload.iteration := iteration
+  
+//   when(io.rsp.fire) {
+//     validrsp := False
+//   }
+// }
 package workshop.mandelbrot
 
 import spinal.core._
@@ -24,47 +94,41 @@ case class PixelResult(g : PixelSolverGenerics) extends Bundle{
 
 case class PixelSolver(g : PixelSolverGenerics) extends Component{
   val io = new Bundle{
-    val cmd = slave  Stream(PixelTask(g))
+    val cmd = slave Stream(PixelTask(g))
     val rsp = master Stream(PixelResult(g))
   }
 
-  val x = Reg(g.fixType) 
-  val y = Reg(g.fixType)
-  val x0 = Reg(g.fixType)
-  val y0 = Reg(g.fixType)
-  val iteration = Reg(g.iterationType) 
-  val two = Reg(g.fixType).allowUnsetRegToAvoidLatch 
-  two := 2
+  import g._
 
-  val start = Reg(Bool) init(False)
-  val validrsp = Reg(Bool) init(False)
-  io.rsp.valid := validrsp
-  
-  when(start && x*x + y*y < 4 && iteration < U(255).resized) {
-    x := (x*x - y*y + x0).truncated
-    y := (x*y*two + y0).truncated
-    iteration := iteration + 1
-  }.elsewhen(start) {
-    start := False
-    validrsp := True
-  }.otherwise{
-    start := False
-  }
+  //Define states
+  val x,y       = Reg(fixType) init(0)
+  val iteration = Reg(iterationType) init(0)
 
+  //Do some shared calculation
+  val xx = x*x
+  val yy = y*y
+  val xy = x*y
 
-  io.cmd.ready := (!io.rsp.valid | io.rsp.ready) && !start
-  when(io.cmd.fire) {
-    start := True
-    x := 0.0
-    y := 0.0
-    iteration := 0
-    x0 := io.cmd.payload.x
-    y0 := io.cmd.payload.y
-  }
+  //Apply default assignement
+  io.cmd.ready := False
+  io.rsp.valid := False
+  io.rsp.iteration := iteration
 
-  io.rsp.payload.iteration := iteration
-  
-  when(io.rsp.fire) {
-    validrsp := False
+  //Is the mandelbrot iteration done ?
+  when(io.cmd.valid) {
+    when(xx + yy >= 4.0 || iteration === iterationLimit) {
+      io.rsp.valid := True
+      when(io.rsp.ready){
+        io.cmd.ready := True
+        x := 0
+        y := 0
+        iteration := 0
+      }
+    } otherwise {
+      x := (xx - yy + io.cmd.x).truncated
+      y := (((xy) << 1) + io.cmd.y).truncated
+      iteration := iteration + 1
+    }
   }
 }
+
--- src/main/scala/workshop/mandelbrot/PixelSolverMultiCore.scala
+++ src/main/scala/workshop/mandelbrot/PixelSolverMultiCore.scala
@@ -9,7 +9,41 @@ case class Dispatcher[T <: Data](dataType : T,outputsCount : Int) extends Compon
     val input = slave Stream(dataType)
     val outputs = Vec(master Stream(dataType),outputsCount)
   }
+
   // TODO
+  // val outputsPayloadReg = Reg(Vec(dataType, outputsCount))
+  // val outputsValidReg = Reg(Vec(Bool, outputsCount))
+  // for(idx <- 0 until outputsCount) {
+  //   io.outputs(idx).valid := outputsValidReg(idx)
+  //   // io.outputs(idx).payloadval counter = Reg(UInt(log2Up(outputsCount) bits)) init(0).assignFromBits(outputsPayloadReg(idx).asBits) 
+  // }
+  // // HIERARCHY VIOLATION : (toplevel/outputsPayloadReg_0_y :  SInt[28 bits]) is driven by (toplevel/dispa/??? :  SInt[28 bits]), but isn't accessible in the dispa component.
+  // //   workshop.mandelbrot.Dispatcher$$anonfun$2.apply$mcV$sp(PixelSolverMultiCore.scala:31)
+  // //   workshop.mandelbrot.Dispatcher.<init>(PixelSolverMultiCore.scala:24)
+  // //   workshop.mandelbrot.PixelSolverMultiCore.<init>(PixelSolverMultiCore.scala:47)
+  // //   workshop.mandelbrot.PixelSolverMultiCoreMain$$anonfun$main$1.apply(PixelSolverMultiCoreMain.scala:9)
+  // //   workshop.mandelbrot.PixelSolverMultiCoreMain$$anonfun$main$1.apply(PixelSolverMultiCoreMain.scala:9)
+  // //   spinal.sim.JvmThread.run(SimManager.scala:51)
+  
+  val counter = Reg(UInt(log2Up(outputsCount) bits)) init(0)
+  io.input.ready := io.outputs(counter).ready
+
+  for(idx <- 0 until outputsCount) {
+    io.outputs(idx).payload := io.input.payload
+    io.outputs(idx).valid := False
+
+    when(counter === idx) {
+      io.outputs(idx).valid := io.input.valid
+
+      when(io.outputs(idx).fire) {
+        when(counter === outputsCount - 1) {
+          counter := 0
+        }.otherwise{
+          counter := counter + 1
+        }
+      }
+    } 
+  }
 }
 
 // TODO Define the Arbiter component (similar to the Dispatcher)
@@ -20,8 +54,48 @@ case class PixelSolverMultiCore(g : PixelSolverGenerics,coreCount : Int) extends
     val rsp = master Stream (PixelResult(g))
   }
 
-  //TODO instantiate all components
+  val counter = Reg(UInt(log2Up(coreCount) bits)) init(0)
+  val dispa = Dispatcher(PixelTask(g), coreCount)
+  dispa.io.input << io.cmd
 
+  val pixelSolverArray = for (i <- 0 until coreCount) yield PixelSolver(g)
+  io.rsp.payload := pixelSolverArray(0).io.rsp.payload
+  io.rsp.valid := False
+
+  for(idx <- 0 until coreCount) {
+    pixelSolverArray(idx).io.cmd << dispa.io.outputs(idx)
+
+    pixelSolverArray(idx).io.rsp.ready := False
+    when(counter === idx) {
+      pixelSolverArray(idx).io.rsp.ready := io.rsp.ready
+      io.rsp.payload := pixelSolverArray(idx).io.rsp.payload
+      io.rsp.valid := pixelSolverArray(idx).io.rsp.valid
+      
+      when(counter === coreCount - 1) {
+        counter := 0
+      }.otherwise{
+        counter := counter + 1
+      }
+    }
+ 
+  //   when(counter === idx) {
+  //     when(pixelSolverArray(idx).io.rsp.valid && !io.rsp.valid) {
+  //       payloadReg := pixelSolverArray(idx).io.rsp.payload
+  //       validReg := True
+  //     }.elsewhen(io.rsp.ready && io.rsp.valid) {
+  //       validReg := False
+
+  //       when(counter === coreCount - 1) {
+  //         counter := 0
+  //       }.otherwise{
+  //         counter := counter + 1
+  //       }
+  //     }
+  //   }
+
+  }
+
+  //TODO instantiate all components
   //TODO interconnect all that stuff
 }
 
