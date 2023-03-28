cc7574be8e887a4ff2c18ef6e160fd508b4a30bf
xgbcat315 <978601499@qq.com>
2023-03-28 11:14:37 +0800

    finished all

--- src/main/scala/workshop/waveplayer/WavePlayer.scala
+++ src/main/scala/workshop/waveplayer/WavePlayer.scala
@@ -24,24 +24,41 @@ case class WavePlayer(generics : WavePlayerGenerics) extends Area{
   val phase = new Area{
     val run = Bool  //Driven later by a WavePlayerMapper (see WavePlayerMapper implementation)
     val rate = UInt(phaseWidth bits) //Driven later by a WavePlayerMapper
-
+    
     //TODO phase
+    val value = Reg(UInt(phaseWidth bits)) init(0)
+    when(run) {
+      value := value + rate
+    }
   }
 
   val sampler = new Area{
     //TODO Rom definition with a sinus + it sampling
+    val romSamples = for(sampleId <- 0 until sampleCount) yield{
+      val sin = Math.sin(2 * Math.PI * sampleId / sampleCount) * ((1 << sampleWidth) - 1) * 0.5 + (1 << sampleWidth-1)
+      BigInt(sin.toInt)
+    }
+    val rom = Mem(generics.Sample, sampleCount) initBigInt(romSamples)
+    val sample = rom.readAsync(phase.value >> (phaseWidth - sampleCountLog2))
   }
 
   val filter = new Area{
-    val bypass = Bool //Driven later by a WavePlayerMapper
-    val coef = UInt(filterCoefWidth bits) //Driven later by a WavePlayerMapper
-    val value = Sample //Output value of the filter Area
-    //TODO first order filter + bypass logic
+    val bypass = Bool
+    val coef = UInt(filterCoefWidth bits)
+    val accumulator = Reg(UInt(sampleWidth + filterCoefWidth bits)) init(0)
+    accumulator := accumulator - (accumulator*coef >> filterCoefWidth) + sampler.sample * coef
+    val filtredSampler = accumulator >> filterCoefWidth
+    val value = bypass ? sampler.sample | filtredSampler
   }
 }
 
 //Area capable to map a WavePlayer on a BusSlaveFactory
 class WavePlayerMapper(bus : BusSlaveFactory, wavePlayer : WavePlayer) extends Area{
-  bus.drive(wavePlayer.phase.run,  address = 0x00) init(False)
+  bus.driveAndRead(wavePlayer.phase.run,  address = 0x00) init(False)
   //TODO phase.rate, phase.value, filter.bypass, filter.coef mapping
+  bus.drive(wavePlayer.phase.rate, address = 0x04) 
+  bus.read(wavePlayer.phase.value, address = 0x08)
+
+  bus.driveAndRead(wavePlayer.filter.bypass,  address = 0x10) init(True)
+  bus.drive(wavePlayer.filter.coef, address = 0x14) init(0)
 }
